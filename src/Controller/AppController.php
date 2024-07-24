d42587689ab103c156778556659cd816589afbf8
Luciana Puccinelli <luciana@oneweb.com.br>
2024-07-24 14:30:54 -0300

    fazendo login para os usuarios

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -37,17 +37,26 @@ class AppController extends Controller
      *
      * @return void
      */
-    public function initialize(): void
+    public function initialize() :void
     {
-        parent::initialize();
-
-        $this->loadComponent('RequestHandler');
         $this->loadComponent('Flash');
+        $this->loadComponent('Auth', [
+            'authenticate' => [
+                'Form' => [
+                    'fields' => [
+                        'username' => 'email',
+                        'password' => 'password'
+                    ]
+                ]
+            ],
+            'loginAction' => [
+                'controller' => 'Users',
+                'action' => 'login'
+            ]
+        ]);
 
-        /*
-         * Enable the following component for recommended CakePHP form protection settings.
-         * see https://book.cakephp.org/4/en/controllers/components/form-protection.html
-         */
-        //$this->loadComponent('FormProtection');
+        // Permite a ação display, assim nosso pages controller
+        // continua a funcionar.
+        $this->Auth->allow(['display']);
     }
 }
