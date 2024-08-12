a0d63a9c268bf8777dfe96fae98cb88d5ff0c1b8
Luciana Puccinelli <luciana@oneweb.com.br>
2024-08-12 10:41:51 -0300

    ajustes nos templates

--- templates/Bookmarks/add.php
+++ templates/Bookmarks/add.php
@@ -22,7 +22,7 @@
                     echo $this->Form->control('title');
                     echo $this->Form->control('description');
                     echo $this->Form->control('url');
-                    echo $this->Form->control($this->Form->input('tag_string', ['type' => 'text']), ['options' => $tags]);
+                    echo $this->Form->control('tags._ids', ['options' => $tags]);
                 ?>
             </fieldset>
             <?= $this->Form->button(__('Submit')) ?>
