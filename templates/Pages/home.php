21d3f3fdef22e0a5b29f2e97ec69dfac42bf657c
Luciana Puccinelli <luciana@oneweb.com.br>
2024-08-12 10:56:15 -0300

    add botao sair

--- templates/Pages/home.php
+++ templates/Pages/home.php
@@ -7,5 +7,6 @@
         <a href="<?= $this->Url->build(['controller' => 'Users', 'action' => 'index']); ?>" class="btn">Users</a>
         <a href="<?= $this->Url->build(['controller' => 'Tags', 'action' => 'index']); ?>" class="btn">Tags</a>
         <a href="<?= $this->Url->build(['controller' => 'Bookmarks', 'action' => 'index']); ?>" class="btn">Bookmarks</a>
+        <a href="<?= $this->Url->build(['controller' => 'Users', 'action' => 'logout']); ?>" class="btn-logout">Sair</a>
     </div>
 </div>
