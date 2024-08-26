dd693bf287a51b993116268053ff743acbc9d6fa
Luciana <luciana@oneweb.com.br>
2024-08-26 13:36:55 -0300

    add icons

--- templates/Pages/home.php
+++ templates/Pages/home.php
@@ -3,9 +3,9 @@
 <div class="container">
     <h1>Escolha o que deseja fazer</h1>
     <div class="button-group">
-        <a href="<?= $this->Url->build(['controller' => 'Users', 'action' => 'index']); ?>" class="btn">Usuários</a>
-        <a href="<?= $this->Url->build(['controller' => 'Tags', 'action' => 'index']); ?>" class="btn">Tags</a>
-        <a href="<?= $this->Url->build(['controller' => 'Bookmarks', 'action' => 'index']); ?>" class="btn">Bookmarks</a>
-        <a href="<?= $this->Url->build(['controller' => 'Bookmarks', 'action' => 'tags']); ?>" class="btn">Procurar bookmarks por tags</a>
+        <a href="<?= $this->Url->build(['controller' => 'Users', 'action' => 'index']); ?>" class="btn"><i class="fa-solid fa-user"></i> Usuários</a>
+        <a href="<?= $this->Url->build(['controller' => 'Tags', 'action' => 'index']); ?>" class="btn"><i class="fa-solid fa-tag"></i> Tags</a>
+        <a href="<?= $this->Url->build(['controller' => 'Bookmarks', 'action' => 'index']); ?>" class="btn"><i class="fa-solid fa-bookmark"></i> Bookmarks</a>
+        <a href="<?= $this->Url->build(['controller' => 'Bookmarks', 'action' => 'tags']); ?>" class="btn"><i class="fa-solid fa-magnifying-glass"></i> Procurar por tags</a>
     </div>
 </div>
