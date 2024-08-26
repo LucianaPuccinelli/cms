72828ccc045015552ab97b6e375f331f5f7dfb96
Luciana <luciana@oneweb.com.br>
2024-08-26 09:51:46 -0300

    ajustando tela de procurar bookmarks por tags

--- templates/Bookmarks/tags.php
+++ templates/Bookmarks/tags.php
@@ -1,23 +1,25 @@
+<?php echo $this->Html->css('search-tags.css'); ?>
 <?= $this->Html->link(__('Voltar'), '/', ['class' => 'button']) ?>
 <h1>
     Bookmarks que tem a tag
-    <?= $this->Text->toList(h($tags)) ?>
+    <strong><?= $this->Text->toList(h($tags)) ?></strong>
 </h1>
-<?= $this->Form->create(null, ['type' => 'get']) ?>
+<?= $this->Form->create(null, ['type' => 'get', 'class' => 'search-form']) ?>
 <?= $this->Form->control('tag', [
     'label' => 'Pesquisar por Tag',
     'options' => $allTags,
-    'value' => $this->request->getQuery('tag')
+    'value' => $this->request->getQuery('tag'),
+    'class' => 'tag-search'
 ]) ?>
 <?= $this->Form->button(__('Pesquisar')) ?>
 <?= $this->Form->end() ?>
-<section>
+
+<section class="bookmarks-section">
     <?php foreach ($bookmarks as $bookmark): ?>
-        <article>
-            <h4><?= $this->Html->link($bookmark->title, '/bookmarks/view/'.$bookmark->id) ?></h4>
-            <small><?= h($bookmark->url) ?></small>
-            <?= $this->Text->autoParagraph(h($bookmark->description)) ?>
-            <br>
+        <article class="bookmark-item">
+            <h4 class="bookmark-title"><?= $this->Html->link($bookmark->title, '/bookmarks/view/'.$bookmark->id) ?></h4>
+            <small class="bookmark-url"><?= h($bookmark->url) ?></small>
+            <p class="bookmark-description"><?= $this->Text->autoParagraph(h($bookmark->description)) ?></p>
         </article>
     <?php endforeach; ?>
 </section>
